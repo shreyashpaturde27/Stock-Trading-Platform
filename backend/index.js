33a532a01d37c79f4fdd42b8bf3e41da515210b3
shreyashpaturde27 <shreyashpaturde@gmail.com>
2026-04-14 15:00:15 +0530

    fix express version

--- backend/index.js
+++ backend/index.js
@@ -203,10 +203,10 @@ app.use(
   "/dashboard",
   express.static(path.join(__dirname, "../dashboard/dist")),
 );
-app.get("/dashboard/:path(*)", (req, res) => {
+app.get("/dashboard/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dashboard/dist/index.html"));
 });
-app.get("/:path(*)", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 app.listen(PORT, () => {
